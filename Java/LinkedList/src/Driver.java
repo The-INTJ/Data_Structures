b7341084bf7a74922769ae9a97c913d825e1db90
Drew Taylor <raylotdreygone@gmail.com>
2019-11-06 18:32:22 -0500

    Added main

---
+++ Java/LinkedList/src/Driver.java
@@ -0,0 +1,12 @@
+public class Driver {
+
+
+    public static void main(String[] args) {
+
+        Node<String> sNode = new Node("a string");
+
+        SingularlyLinkedList sll = new SingularlyLinkedList(sNode);
+
+    }
+
+}
